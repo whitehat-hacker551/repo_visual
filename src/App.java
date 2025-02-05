3e69c0e7c8cebdc056e9fd9329d949b94dcda8a2
whitehat-hacker551 <dcalinescu00@gmail.com>
2025-02-05 13:02:27 +0100

    Hola

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Introduce nombre:");
         String nombre=entrada.nextLine();
-        System.out.println("Te miro y te cambio")
+        System.out.println("Te miro y te cambio");
 
     }
 }
