8a5489ea047abe26308937603009a020f0b2a8c5
whitehat-hacker551 <dcalinescu00@gmail.com>
2025-02-05 12:44:23 +0100

    He añadido un objeto Scanner y le he metido un valor de entrada

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Hello, World!");
+        System.out.println("Introduce nombre:");
+        String nombre=entrada.nextLine();
+
+    }
+}
